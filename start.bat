f5e0dece2128c0ed5ee3c352625cb6fdbd248ed3
TS-DEV-PYTHON <christineeffat88@gmail.com>
2025-07-24 01:46:04 +0300

    Update start.bat

--- start.bat
+++ start.bat
@@ -2,6 +2,6 @@
 color a
 echo starting poke-dl
 echo Source avaliable at: https://github.com/TS-DEV-JAVA/Poke-dl
-echo Data json at: https://files.catbox.moe/z8a8i9.json
+echo Data json at: https://raw.githubusercontent.com/TS-DEV-JAVA/Poke-dl-api/refs/heads/main/Api-db.json
 python main-v2.py
 pause

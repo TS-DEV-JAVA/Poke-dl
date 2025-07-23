ee58b2158a93688ed7eac5feeeaa7a3c5eb8cfa2
TS-DEV-PYTHON <christineeffat88@gmail.com>
2025-07-23 16:34:51 +0300

    Add files via upload

---
+++ Install-needs.bat
@@ -0,0 +1,5 @@
+@echo off
+echo installing needed python modules...
+pip install requests colorama
+echo done !
+pause
---
+++ start.bat
@@ -0,0 +1,7 @@
+@echo off
+color a
+echo starting poke-dl
+echo Source avaliable at: coming soon
+echo Data json at: https://files.catbox.moe/z8a8i9.json
+python main-v2.py
+pause

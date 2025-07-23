ef2a0e27cb0573f9014e9106b554c8443cf02a03
TS-DEV-PYTHON <christineeffat88@gmail.com>
2025-07-23 16:37:37 +0300

    Update start.bat

--- start.bat
+++ start.bat
@@ -1,7 +1,7 @@
 @echo off
 color a
 echo starting poke-dl
-echo Source avaliable at: coming soon
+echo Source avaliable at: https://github.com/TS-DEV-JAVA/Poke-dl
 echo Data json at: https://files.catbox.moe/z8a8i9.json
 python main-v2.py
 pause

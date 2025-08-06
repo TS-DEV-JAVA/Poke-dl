da507d0b098c1b9449925f5ab73dfe3db4f04989
TS-DEV-PYTHON <christineeffat88@gmail.com>
2025-08-06 13:28:05 +0300

    Delete start.bat

--- start.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-color a
-echo starting poke-dl
-echo Source avaliable at: https://github.com/TS-DEV-JAVA/Poke-dl
-echo Data json at: https://raw.githubusercontent.com/TS-DEV-JAVA/Poke-dl-api/refs/heads/main/Api-db.json
-python main-v2.py
-pause
